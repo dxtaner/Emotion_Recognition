4d54fdf1b9a3383233016c96218fb0d1799a1d97
taner <tanerozer16@gmail.com>
2023-08-10 13:20:17 +0300

    Create emojidetect.py

---
+++ EmotionDetect/emojidetect.py
@@ -0,0 +1,55 @@
+import cv2
+import cvlib as cv
+import numpy as np
+from keras.models import load_model
+from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array
+
+model_best = load_model("emotionModel.h5")
+font = cv2.FONT_HERSHEY_DUPLEX
+
+emotion_dict = {
+    0: 'Kizgin',
+    1: 'Igrenme',
+    2: 'Korku',
+    3: 'Mutlu',
+    4: 'Uzgun',
+    5: 'Sasirma',
+    6: 'Dogal'
+}
+
+kamera = cv2.VideoCapture(0)  # 0, yerel kamera için kullanılır. Farklı bir cihaz kullanıyorsanız, cihazın indeksini belirtmelisiniz.
+
+while kamera.isOpened():
+    ret, frame = kamera.read()
+
+    yuz, confidence = cv.detect_face(frame)
+
+    tonlama = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # tonlama ayarı
+
+    for idx, face in enumerate(yuz):
+        (x, y) = face[0], face[1]
+        (w, h) = face[2], face[3]
+
+        cv2.rectangle(frame, (x, y), (w, h), (0, 0, 255), 3)
+
+        image_c = np.copy(tonlama[y:h, x:w])
+
+        if (image_c.shape[0]) < 10 or (image_c.shape[1]) < 10:
+            continue
+
+        center_img = np.array(Image.fromarray(image_c).resize([48, 48]))
+        face_img = cv2.cvtColor(center_img, cv2.COLOR_GRAY2RGB)
+        face_img = np.array(Image.fromarray(center_img).resize([48, 48]))
+        results = model_best.predict(face_img.reshape(1, 48, 48, 1))
+
+        cv2.putText(frame, 'Durum:{}({:.2f})'.format(emotion_dict[np.argmax(results)], np.max(results)),
+                    (x, y - 10), font, 0.6, (0, 255, 0), 2)
+
+    cv2.imshow("Webcam", frame)
+
+    if (cv2.waitKey(1) & 0xFF == ord('q')):  # 30 milisaniye beklemek yerine her 1 milisaniyede bir kontrol eder.
+        break
+
+kamera.release()
+cv2.destroyAllWindows()
